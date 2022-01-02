a6d8372a411f7d91f6c5743c5112fcabd5da3ab4
Pawel Chomicki <pawel.chomicki@gmail.com>
2022-01-02 10:25:06 +0100

    Create smaller conda package

--- conda/build.sh
+++ conda/build.sh
@@ -1,2 +1,8 @@
-poetry install
+echo "############# START ###################"
+wget -O -  https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+source $HOME/.poetry/env
+rm -rf poetry.lock
+poetry install --no-dev --no-root
 poetry build
+pip install --no-deps dist/*.whl
+echo "############# END ###################"
